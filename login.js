0e573708b943861c6cfc8b4ab8e02c644b6b6c52
Decal <vumublne@naver.com>
2025-02-04 18:07:35 +0900

    Add eye toggle switch

--- login.js
+++ login.js
@@ -1,8 +1,9 @@
+// 로그인 버튼 활성화
 const btn = document.querySelector('.btn');
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
 
-function btnToggle(e) {
+function btnToggle() {
   if (email.value && password.value) {
     btn.classList.add('btnAble');
     btn.disabled = false;
@@ -14,3 +15,19 @@ function btnToggle(e) {
 
 email.addEventListener('keyup', btnToggle);
 password.addEventListener('keyup', btnToggle);
+
+// 비밀번호 표시(눈 아이콘)
+const eyeIcon = document.querySelectorAll('.eye-icon');
+
+function iconShow() {
+  eyeIcon[1].classList.toggle('off');
+  password.type = 'password';
+}
+
+function iconHidden() {
+  eyeIcon[1].classList.toggle('off');
+  password.type = 'text';
+} 
+
+eyeIcon[0].addEventListener('click', iconShow);
+eyeIcon[1].addEventListener('click', iconHidden);
--- signup.js
+++ signup.js
@@ -1,3 +1,4 @@
+// 로그인 버튼 활성화
 const btn = document.querySelector('.btn');
 const email = document.querySelector('#email');
 const nickname = document.querySelector('#nickname');
@@ -19,3 +20,26 @@ email.addEventListener('keyup', btnToggle);
 nickname.addEventListener('keyup', btnToggle);
 password.addEventListener('keyup', btnToggle);
 passwordCheck.addEventListener('keyup', btnToggle);
+
+// 비밀번호 표시(눈 아이콘)
+const eyeIcon = document.querySelectorAll('.eye-icon');
+
+function eyeOn() {
+  eyeIcon[1].classList.toggle('off');
+  eyeIcon[3].classList.toggle('off');
+  password.type = 'password';
+  passwordCheck.type = 'password';
+}
+
+function eyeOff() {
+  eyeIcon[1].classList.toggle('off');
+  eyeIcon[3].classList.toggle('off');
+  password.type = 'text';
+  passwordCheck.type = 'text';
+} 
+
+
+eyeIcon[0].addEventListener('click', eyeOn);
+eyeIcon[1].addEventListener('click', eyeOff);
+eyeIcon[2].addEventListener('click', eyeOn);
+eyeIcon[3].addEventListener('click', eyeOff);
