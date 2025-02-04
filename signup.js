880e5039cdc3d129770049b4dad28ed6c639d985
Decal <vumublne@naver.com>
2025-02-04 14:18:23 +0900

    로그인btn 활성화 추가, 구글 애널리틱스 전체적용

--- signup.js
+++ signup.js
@@ -1,9 +1,12 @@
 const btn = document.querySelector('.btn');
 const email = document.querySelector('#email');
+const nickname = document.querySelector('#nickname');
+const passwordCheck = document.querySelector('#passwordCheck');
 const password = document.querySelector('#password');
 
+
 function btnToggle(e) {
-  if (email.value && password.value) {
+  if (email.value && nickname.value && password.value && passwordCheck.value) {
     btn.classList.add('btnAble');
     btn.disabled = false;
   } else {
@@ -13,4 +16,6 @@ function btnToggle(e) {
 }
 
 email.addEventListener('keyup', btnToggle);
+nickname.addEventListener('keyup', btnToggle);
 password.addEventListener('keyup', btnToggle);
+passwordCheck.addEventListener('keyup', btnToggle);
