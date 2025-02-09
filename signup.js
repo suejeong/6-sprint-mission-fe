9389baa27e8780f08ac3ac95c5a74896b61bbf75
xdnjs7 <xdnjs77777@gmail.com>
2025-02-09 23:17:15 +0900

    Add Password Visibility Toggle Btn

--- signup.js
+++ signup.js
@@ -1,5 +1,7 @@
+//로그인 버튼 색깔 변경
 const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
+const checkPasswordInput = document.getElementById('checkpassword');
 const submitBtn = document.getElementById('submitBtn');
 
 function updateSubmitButton() {
@@ -12,3 +14,22 @@ function updateSubmitButton() {
 
 emailInput.addEventListener('input', updateSubmitButton);
 passwordInput.addEventListener('input', updateSubmitButton);
+
+
+//비밀번호 표시 토글
+const togglePasswordBtn = document.getElementById('togglePassword');
+const toggleCheckPasswordBtn = document.getElementById('toggleCheckPassword');
+
+
+function togglePasswordVisibility(input, button) {
+    if (input.type === 'password') {
+        input.type = 'text';
+        button.src = 'btn_visibility_on.png';
+    } else {
+        input.type = 'password';
+        button.src = 'btn_visibility_off.png';
+    }
+}
+
+togglePasswordBtn.addEventListener('click', () => togglePasswordVisibility(passwordInput, togglePasswordBtn));
+toggleCheckPasswordBtn.addEventListener('click', () => togglePasswordVisibility(checkPasswordInput, toggleCheckPasswordBtn));
